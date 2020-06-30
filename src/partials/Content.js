92aac471f9d662f7eaf6a168d0c393c55c88f579
miachiu <mia.chiu@collbitx.com>
2020-06-30 10:13:04 +0800

    Update with new front end version

--- src/partials/Content.js
+++ src/partials/Content.js
@@ -1,70 +1,68 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import StepConnector from '@material-ui/core/StepConnector';
-import clsx from 'clsx';
-import Check from '@material-ui/icons/Check';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Bridge from '../components/Info';
-import Originator from '../components/OriVASP';
-import BeneInfo from '../components/BeneInfo/BeneInfo';
-import OriginInfo from '../components/OriVasp/OriginInfo'; //測試用
-import 'typeface-noto-sans';
-import 'typeface-open-sans';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import StepConnector from "@material-ui/core/StepConnector";
+import clsx from "clsx";
+import Check from "@material-ui/icons/Check";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import { Tab, Tabs } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Bridge from "../components/Bridge";
+import Originator from "../components/OriVASP";
+import BeneInfo from "../components/BeneInfo/BeneInfo";
+import "typeface-noto-sans";
+import "typeface-open-sans";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     //flexGrow: 1,
-    height: '100%',
-    borderRadius: '0',
+    height: "100%",
+    borderRadius: "0",
   },
   stepBlock: {
-    padding: '22px 0 33px',
+    padding: "22px 0 33px",
   },
   stepStyle: {
-    width: '100%',
-    maxWidth: '640px',
-    margin: '0 auto',
-    padding: '0',
-    marginTop: '22px',
-    backgroundColor: 'transparent',
+    width: "100%",
+    maxWidth: "640px",
+    margin: "0 auto",
+    padding: "0",
+    marginTop: "22px",
+    backgroundColor: "transparent",
   },
   leftPadding: {
-    padding: '20px',
+    padding: "20px",
+    borderRadius: "0",
   },
   rightPadding: {
-    padding: '20px 30px',
+    padding: "20px 30px",
   },
   textCenter: {
-    textAlign: 'center',
+    textAlign: "center",
   },
 }));
 
 const QontoConnector = withStyles({
   alternativeLabel: {
     top: 12,
-    left: 'calc(-50% + 16px)',
-    right: 'calc(50% + 16px)',
+    left: "calc(-50% + 16px)",
+    right: "calc(50% + 16px)",
   },
   active: {
-    '& $line': {
-      borderColor: '#42826B',
+    "& $line": {
+      borderColor: "#42826B",
     },
   },
   completed: {
-    '& $line': {
-      borderColor: '#42826B',
+    "& $line": {
+      borderColor: "#42826B",
     },
   },
   line: {
-    borderColor: '#9FB6AE',
+    borderColor: "#9FB6AE",
     borderTopWidth: 5,
     borderRadius: 1,
   },
@@ -72,24 +70,35 @@ const QontoConnector = withStyles({
 
 const useQontoStepIconStyles = makeStyles({
   root: {
-    color: '#9FB6AE',
-    display: 'flex',
+    color: "#9FB6AE",
+    display: "flex",
     height: 30,
-    alignItems: 'center',
+    alignItems: "center",
   },
   active: {
-    color: '#42826B',
+    color: "#42826B",
   },
   circle: {
     width: 30,
     height: 30,
-    borderRadius: '50%',
-    backgroundColor: 'currentColor',
+    borderRadius: "50%",
+    backgroundColor: "currentColor",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   completed: {
-    color: '#104935',
+    color: "#fff",
     zIndex: 1,
     fontSize: 18,
+    width: 30,
+    height: 30,
+    backgroundColor: "#104935",
+    borderRadius: "50%",
+    svg: {
+      width: 24,
+      height: 24,
+    },
   },
 });
 
@@ -102,53 +111,44 @@ function QontoStepIcon(props) {
         [classes.active]: active,
       })}
     >
-      {completed ? <Check className={classes.completed} /> : <div className={classes.circle} />}
+      {completed ? (
+        <div className={classes.circle}>
+          <Check className={classes.completed} />
+        </div>
+      ) : (
+        <div className={classes.circle} />
+      )}
     </div>
   );
 }
 
 function getSteps() {
-  return ['', '', '', ''];
-}
-
-function getStepContent(getSteps) {
-  switch (getSteps) {
-    case 0:
-      return <Originator />;
-    case 1:
-      return <BeneInfo />;
-    case 2:
-      return <Originator />;
-    case 3:
-      return <Originator />;
-    default:
-      throw new Error('Unknown step');
-  }
+  return ["", "", "", ""];
 }
 
 const StyledTabs = withStyles({
+  root: {
+    justifyContent: "center",
+  },
   indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(16, 73, 53, 0.32);',
-    '& > span': {
-      maxWidth: 40,
-      width: '100%',
-      backgroundColor: '#104935',
-    },
+    width: "100%",
+    height: "8px",
+    backgroundColor: "rgba(16, 73, 53, 1)",
+    borderRadius: "20px",
   },
 })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
 
 const StyledTab = withStyles((theme) => ({
   root: {
-    textTransform: 'none',
-    color: '#222B45',
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: theme.typography.pxToRem(15),
-    margin: '0 auto',
-    '&:focus': {
+    textTransform: "none",
+    color: "#222B45",
+    fontSize: "18px",
+    padding: "25px 18px",
+    borderBottom: "5px solid rgba(16, 73, 53, 0.32)",
+    "&:focus": {
       opacity: 1,
     },
+    cursor: "default",
   },
 }))((props) => <Tab disableRipple {...props} />);
 
@@ -156,16 +156,57 @@ function Content() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
-  const handleNext = () => {
+  const [value, setValue] = React.useState(0);
+  const [save, setSave] = React.useState("");
+  const [clickAccept, setClickAccept] = React.useState(false);
+  const [disable, setDisable] = React.useState(false);
+  console.log(`save2 = ${save}`);
+  const handleSend = () => {
+    setValue(value + 1);
     setActiveStep(activeStep + 1);
+    setSave(save);
+  };
+  const handleDycrypt = () => {
+    setActiveStep(activeStep + 1);
+  };
+  const handleAccept = () => {
+    setValue(0);
+    setActiveStep(activeStep + 1);
+    setClickAccept(true);
+    setDisable(true);
+  };
+  const handleReject = () => {
+    setValue(0);
+    setActiveStep(activeStep + 1);
+    setDisable(true);
   };
   const handleBack = () => {
     setActiveStep(activeStep - 1);
   };
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  function getStepContent(getSteps) {
+    switch (getSteps) {
+      case 0:
+        return (
+          <Originator
+            onSend={handleSend}
+            activeStep={activeStep}
+            clickAccept={clickAccept}
+            disable={disable}
+            save={save}
+          />
+        );
+      case 1:
+        return (
+          <BeneInfo
+            onDycrypt={handleDycrypt}
+            onAccept={handleAccept}
+            onReject={handleReject}
+          />
+        );
+      default:
+        throw new Error("Unknown step");
+    }
+  }
   return (
     <React.Fragment>
       <div className="container">
@@ -174,53 +215,50 @@ function Content() {
             <Typography variant="h5" className={classes.textCenter}>
               Description text
             </Typography>
-            <Stepper alternativeLabel activeStep={activeStep} connector={<QontoConnector />} className={classes.stepStyle}>
+            <Stepper
+              alternativeLabel
+              activeStep={activeStep}
+              connector={<QontoConnector />}
+              className={classes.stepStyle}
+            >
               {steps.map((label) => (
-                <Step key={label}>
-                  <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+                <Step
+                  key={label}
+                  onClick={() => {
+                    if (activeStep > 0) {
+                      setActiveStep(activeStep - 1);
+                    }
+                  }}
+                >
+                  <StepLabel StepIconComponent={QontoStepIcon}>
+                    {label}
+                  </StepLabel>
                 </Step>
               ))}
             </Stepper>
+            {console.log(`activeStep= ${activeStep}`)}
           </div>
           <Grid container spacing={3}>
             {/* VASP & Info */}
             <Grid item xs={12} md={8}>
-              <Paper elevation={0} className={classes.root, classes.leftPadding}>
+              <Paper
+                elevation={0}
+                className={(classes.root, classes.leftPadding)}
+              >
                 <div>
-                  <StyledTabs value={value} onChange={handleChange}>
-                    <StyledTab label="Originator VASP" />
-                    <StyledTab label="Beneficiary Info" />
+                  <StyledTabs value={value} centered disable>
+                    <StyledTab label="Originator VASP" value={0} />
+                    <StyledTab label="Beneficiary Info" value={1} />
                   </StyledTabs>
                   <Typography className={classes.padding} />
                 </div>
-                {getStepContent(activeStep)}
-                <div className={classes.buttons}>
-                  {/* {activeStep !== 0 && (
-                    <Button onClick={handleBack} className={classes.button}>
-                      Back
-                    </Button>
-                  )} */}
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
-                  >
-                    {activeStep === 0 ? 'Send' : 'Accept'}
-                    {/* {activeStep === 2 ? 'test' :'Accept'} */}
-                  </Button>
-                  {activeStep === 2 && (
-                      <Button onClick={handleNext} className={classes.button}>
-                        Reject
-                      </Button>
-                  )} 
-                </div>
-              </Paper>  
+                {getStepContent(value)}
+              </Paper>
             </Grid>
             {/* Bridge Service */}
             <Grid item xs={12} md={4}>
               <Paper elevation={0} className={classes.root}>
-                <Bridge />
+                <Bridge activeStep={activeStep} clickAccept={clickAccept} />
               </Paper>
             </Grid>
           </Grid>
