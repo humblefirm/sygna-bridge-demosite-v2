640533bce151573d155efb631c34153babe7f8b2
kunming.liu <kunming@coolbitx.com>
2020-07-05 23:06:05 +0800

    Updates

---
+++ precache-manifest.0dbf445dbd1b3b17cbfb3302e033a820.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85739d92709506d3b42ccccd827cf67",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/index.html"
+  },
+  {
+    "revision": "e8c798c80245c409c4f4",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/css/2.fa374d3f.chunk.css"
+  },
+  {
+    "revision": "a5807559b20e55d9e9ab",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/css/main.68865fd2.chunk.css"
+  },
+  {
+    "revision": "e8c798c80245c409c4f4",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/js/2.4d5d8faf.chunk.js"
+  },
+  {
+    "revision": "4ab85d8c00ac0e4a6b9f0e03c7b60b5e",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/js/2.4d5d8faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5807559b20e55d9e9ab",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/js/main.4bed2900.chunk.js"
+  },
+  {
+    "revision": "2368d9bc42d82ccb2d0f",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/js/runtime-main.17e3d246.js"
+  },
+  {
+    "revision": "50c0a88fa13f2005a4c2e83119af35ce",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/logo.50c0a88f.svg"
+  },
+  {
+    "revision": "175853ded2a4e2249d37c46ca0ca33c5",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-400.175853de.woff2"
+  },
+  {
+    "revision": "e7ccb0aa43af9fb7fae4d1bdc23a099b",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-400.e7ccb0aa.woff"
+  },
+  {
+    "revision": "14425b1ebbce551302a950b083b6c14e",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-400italic.14425b1e.woff"
+  },
+  {
+    "revision": "baa3ec09eb827b4f10593032d1cf2a88",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-400italic.baa3ec09.woff2"
+  },
+  {
+    "revision": "43750a193b8ff0b4e929323a72d79301",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-700.43750a19.woff2"
+  },
+  {
+    "revision": "a8edfe7bbf3b01666adc30c118cc5223",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-700.a8edfe7b.woff"
+  },
+  {
+    "revision": "0e19737ff39f96eb1ff99e463fa457a4",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-700italic.0e19737f.woff"
+  },
+  {
+    "revision": "bc98820a9bf62004a88ef07e012e3ce3",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/noto-sans-latin-700italic.bc98820a.woff2"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/CoolBitX-Technology/sygna-bridge-demosite-v2/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
